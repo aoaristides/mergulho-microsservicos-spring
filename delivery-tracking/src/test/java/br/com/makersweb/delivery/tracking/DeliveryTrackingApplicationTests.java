4413e87a4023cd929392b6cf60fcbd5decd50b81
Anderson Orione Aristides <andersonorione@MacBook-Pro-de-Anderson.local>
2026-01-19 23:14:01 -0300

    feat: Criado projetos de microsservços

---
+++ delivery-tracking/src/test/java/br/com/makersweb/delivery/tracking/DeliveryTrackingApplicationTests.java
@@ -0,0 +1,13 @@
+package br.com.makersweb.delivery.tracking;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DeliveryTrackingApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
